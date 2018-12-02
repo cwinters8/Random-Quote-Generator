b3c60899f78dc6fadc3a16525b947ec21f35d0a7
cwinters8 <clwinters8@gmail.com>
2018-12-01 19:25:48 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// quote data
+let quotes = [
+    {
+        quote: 'Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.',
+        source: 'Patrick McKenzie'
+    },
+    {
+        quote: 'Injustice anywhere is a threat to justice everywhere.',
+        source: 'Martin Luther King Jr.'
+    },
+    {
+        quote: 'I am rarely happier than when spending an entire day programming my computer to perform automatically a task that would otherwise take me a good ten seconds to do by hand.',
+        source: 'Douglas Adams'
+    },
+    {
+        quote: 'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
+        source: 'Douglas Adams'
+    },
+    {
+        quote: 'Do or do not, there is no try.',
+        source: 'Yoda'
+    },
+]
+console.log(quotes);
